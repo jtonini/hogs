1e516062bd6e46a880eb034f7af33205869fc508
cazuza <cazuza@badenpowell.richmond.edu>
2024-04-10 15:57:49 -0400

    adding sql file to repo

---
+++ hogs.sql
@@ -0,0 +1,8 @@
+CREATE TABLE hogs (
+        t datetime default current_timestamp,
+        host varchar(20),
+        cpu_used varchar(20),
+        cpu_total varchar(20),
+        mem_used varchar(20),
+        mem_total varchar(20)
+        );
